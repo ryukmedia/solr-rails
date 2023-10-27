5b412faf98c0decd410b07f8677d88b01712cb56
ryukmedia <149170312+ryukmedia@users.noreply.github.com>
2023-10-27 10:47:11 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
+  #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    collection do
+      post :search  # creates a route called users_search
+    end
+  end
+
 end
+
+
